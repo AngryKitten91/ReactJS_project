7f86aed43caad5fd528715a889ecba2ada14b4fd
Bart <bartkof91@gmail.com>
2024-06-18 23:33:20 +0200

    fixed gh pages base route

--- src/App.jsx
+++ src/App.jsx
@@ -10,52 +10,60 @@ import { JobsDirectoryScreen } from "./screens/JobsDirectory/JobsDirectoryScreen
 import { ProtectedRoute } from "./providers/ProtectedRoute.jsx";
 import { Navigate } from "react-router-dom";
 
-const router = createBrowserRouter([
+// added for git hub pages purposes
+const BASE_URL_NAME = "ReactJS_project";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <LoginScreen />,
+    },
+    {
+      path: routes.login.path,
+      element: <LoginScreen />,
+    },
+    {
+      path: routes.register.path,
+      element: <RegisterScreen />,
+    },
+    {
+      path: routes.reset.path,
+      element: <ResetScreen />,
+    },
+    {
+      path: routes.dashboard.path,
+      element: (
+        <ProtectedRoute>
+          <Dashboard />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: routes.users.path,
+      element: (
+        <ProtectedRoute>
+          <UsersManagementScreen />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: routes.jobs.path,
+      element: (
+        <ProtectedRoute>
+          <JobsDirectoryScreen />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: "/*",
+      element: <Navigate to={routes.login.path} />,
+    },
+  ],
   {
-    path: "/",
-    element: <LoginScreen />,
-  },
-  {
-    path: routes.login.path,
-    element: <LoginScreen />,
-  },
-  {
-    path: routes.register.path,
-    element: <RegisterScreen />,
-  },
-  {
-    path: routes.reset.path,
-    element: <ResetScreen />,
-  },
-  {
-    path: routes.dashboard.path,
-    element: (
-      <ProtectedRoute>
-        <Dashboard />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: routes.users.path,
-    element: (
-      <ProtectedRoute>
-        <UsersManagementScreen />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: routes.jobs.path,
-    element: (
-      <ProtectedRoute>
-        <JobsDirectoryScreen />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "*",
-    element: <Navigate to={routes.login.path} />,
-  },
-]);
+    basename: `/${BASE_URL_NAME}`,
+  }
+);
 
 function App() {
   return (
